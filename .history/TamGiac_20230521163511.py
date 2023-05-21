53d1a6c4f82be12692b18c43b1ebcc397c65a7a0
LyNhatKhanh <lynhatkhanh.2001@gmail.com>
2023-05-21 16:35:48 +0700

    add tamgiac method

---
+++ .history/TamGiac_20230521163511.py
@@ -0,0 +1,23 @@
+import math
+
+def ktra_hop_le(a,b,c):
+    if a == 0 or b == 0 or c == 0:
+        return 1
+    elif a+b < c or a+c < b or b+c < a:
+        return 1
+    else:   return 2
+
+
+
+def cac_phep_tinh_TG(a,b,c):
+    cv = (a+b+c)
+    p = cv/2
+    dt = '{0:.{1}f}'.format(math.sqrt(p*(p-a)*(p-b)*(p-c)),2)
+    print("Chu vi:",cv)
+    print("p:",p)
+    print("Dien tich:",dt)
+
+a=int(input("Nhap canh a: "))
+b=int(input("Nhap canh b: "))
+c=int(input("Nhap canh c: "))
+
